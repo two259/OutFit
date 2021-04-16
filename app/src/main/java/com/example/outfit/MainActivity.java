6e906f5b616e14e9ebe7db190973f9fc5eece4ef
two259 <two259@vt.edu>
2021-04-16 15:34:26 -0400

    Added login and registration screens.

--- app/src/main/java/com/example/outfit/MainActivity.java
+++ app/src/main/java/com/example/outfit/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.outfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button signUpButton;
+    EditText usernameText;
+    EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+
     }
 }
